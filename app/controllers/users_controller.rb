59034352eaae406d8b9477c2d954c522a272583a
mikecave <mikecave@gmail.com>
2013-09-02 18:55:54 +0100

    completed chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to Mike's Twitter!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+  @user = User.new(user_params)
+  if @user.save
+    sign_in @user
+    flash[:success] = "Welcome to the Sample App!"
+    redirect_to @user
+  else
+    render 'new'
   end
+end
 
   private
 
