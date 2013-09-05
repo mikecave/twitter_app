e6d462f935d52fed425a499a1c10d1f46191ead9
mikecave <mikecave@gmail.com>
2013-09-05 23:04:16 +0100

    finished 10.3.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,6 +63,7 @@ describe "Authentication" do
 
     describe "authorization" do
 
+
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
 
@@ -76,6 +77,17 @@ describe "Authentication" do
           describe "submitting to the update action" do
             before { patch user_path(user) }
             specify { expect(response).to redirect_to(signin_path) }
+          end
+          describe "in the Microposts controller" do
+            describe "submitting to the create action" do
+              before { post microposts_path }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before { delete micropost_path(FactoryGirl.create(:micropost)) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
           end
           
           describe "when attempting to visit a protected page" do
@@ -107,10 +119,7 @@ describe "Authentication" do
           end
         end
       end
-
-
-
-      end
+    end
 
       describe "as wrong user" do
         let(:user) { FactoryGirl.create(:user) }
