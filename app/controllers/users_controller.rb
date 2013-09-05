7ca4d78a4919cd854f6c64109962f5acbe320d14
mikecave <mikecave@gmail.com>
2013-09-05 18:33:52 +0100

    finished 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  before_filter :signed_in_user_filter, only: [:new, :create]
+
   def show
     @user = User.find(params[:id])
   end
@@ -32,9 +35,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+    end
+    redirect_to users_url
+    end
 
   private
 
@@ -56,4 +69,14 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+    def signed_in_user_filter
+    if signed_in?
+        redirect_to root_path, notice: "Already logged in"
+    end
+end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -93,27 +93,71 @@ describe "Authentication" do
                 end
               end
           end
-          describe "visiting the user index" do
-            before { visit users_path }
-            it { should have_title('Sign in') }
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+          it "should render the default (profile) page" do
+            expect(page).to have_title(user.name)
           end
         end
       end
 
+
+
+      end
+
       describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user, no_capybara: true }
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+        before { sign_in user, no_capybara: true }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_title(full_title('Edit user')) }
+        describe "visiting Users#edit page" do
+          before { visit edit_user_path(wrong_user) }
+          it { should_not have_title(full_title('Edit user')) }
+        end
+
+        describe "submitting a PATCH request to the Users#update action" do
+          before { patch user_path(wrong_user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+      
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin, no_capybara: true }
+
+        describe "submitting a DELETE request to the Users#destroy action" do
+          before { delete user_path(user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+      
+      describe "using a 'new' action" do
+                let(:user) { FactoryGirl.create(:user) }
+                  
+                before do
+                  sign_in user,  no_capybara: true
+                get new_user_path 
+                end
+                specify { response.should redirect_to(root_path) }
       end
 
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
-    end
+      describe "using a 'create' action" do
+        let(:user) { FactoryGirl.create(:user) }
+                  
+                before do
+                  sign_in user,  no_capybara: true
+                  post users_path 
+                end
+        specify { response.should redirect_to(root_path) }
+      end   
     end
 end
